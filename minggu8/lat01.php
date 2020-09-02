9b3fdf7669e6222025a42d234c961198e181d61c
Rangga Dwi H <68285557+RanggaDwiH@users.noreply.github.com>
2020-09-02 17:20:48 +0700

    Add files via upload

---
+++ minggu8/lat01.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Belajar PHP";
+echo '<br>';
+echo 'saya siswa SMK';
+echo '<h1 style="background-color:red">Belajar PHP itu mudah</h1>
+        <p style="background-color:blue">Saya suka belajar permrograman</p>';
+
+echo 'saya belajar', 'php', 'br';
+?>
---
+++ minggu8/lat04.php
@@ -0,0 +1,55 @@
+<?php 
+
+//opartor matematika
+
+$a = 5;
+$b = 2;
+
+$c = $a + $b;
+echo $c. '<br>';
+
+$c = $a - $b;
+echo $c.'<br>';
+
+$c = $a * $b;
+echo $c.'<br>';
+
+$c = $a / $b;
+echo $c.'<br>';
+;
+$c = $a % $b;
+echo $c.'<br>';
+
+
+//operator logika
+
+$c = $a < $b;
+echo $c.'<br>';
+
+$c = $a > $b;
+echo $c.'<br>';
+
+$c = $a == $b;
+echo $c.'<br>';
+
+$c = $a != $b;
+echo $c.'<br>';
+
+//increment
+
+$a++;
+echo $a.'<br>';
+
+//operator string
+
+$kata = 'sura';
+$kota = 'baya';
+
+$hasil = $kata.$kota;
+
+$hasil .= 'Kota Pahlawan';
+echo $hasil;
+
+
+
+?>
